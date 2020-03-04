7fc038478c95e7800a2804cfc0f28f06d2f19b48
kangjing-js <18538079040@163.com>
2020-03-04 18:18:06 +0800

    3.4

---
+++ server/index.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+app.use(require('cors')())
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/lkj-blog',
+{ useNewUrlParser: true ,useUnifiedTopology: true})
+
+const Blog = mongoose.model('Blog',new mongoose.Schema({
+    title:{ 
+        type:String},
+    content:{
+        type:String}
+}))
+
+
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+//新建博客接口
+app.post('/blog',async(req,res)=>{
+    console.log(req.body);
+    const result = await Blog.create(req.body);
+    console.log(result);
+    res.send(result)
+})
+
+//博客列表接口
+app.get('/bloglist',async(req,res)=>{
+    const result = await Blog.find();
+    res.send(result)
+})
+
+//查询一条博客
+app.get('/bloglist/:id',async(req,res)=>{
+    const result = await Blog.findById(req.params.id);
+    res.send(result)
+})
+
+//删除一条博客
+app.delete('/blog/:id',async (req,res)=>{
+    await Blog.findByIdAndDelete(req.params.id)
+    res.send({
+        success:true
+    })
+})
+//修改一条博客
+app.put('/blog/:id',async (req,res)=>{
+    await Blog.findByIdAndUpdate(req.params.id,req.body)
+    res.send({
+        success:true
+    })
+})
+
+
+app.listen(3000, () => console.log(`Example app listening on port 3000!`))
